95bb91c6039cbf47ddbb2a8064796e4f5bc6c5cf
Lewis <LewisArdern@live.co.uk>
2019-05-15 00:10:57 -0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
-document.getElementById("demo1").innerHTML = location.hash.match(/demo1=([^;&]*)/)[1];
-
+if (location.hash.match(/demo1=([^;&]*)/)) {
+    document.getElementById("demo1").innerHTML = unescape(location.hash.match(/demo1=([^;&]*)/)[1]);
+}
 document.getElementById("demo3").innerHTML = 5;
 document.getElementById("demo4").innerHTML = "<a href='/about.html'>About</a>";
 
@@ -75,6 +76,6 @@ document.getElementById("demo4").innerHTML = "<a href='/about.html'>About</a>";
 
 
 
-if (location.hash.match(/demo2=([^;&]*)/)[1]) {
+if (location.hash.match(/demo2=([^;&]*)/)) {
     location.href = location.hash.match(/demo2=([^;&]*)/)[1];
 }
