fb11a0bd5cc4e36f9bf08e7bd87b1d3dca0b54bf
Lewis <LewisArdern@live.co.uk>
2019-05-14 23:54:05 -0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,14 +1,80 @@
-var input = ['value',window.location.search.substring(2)]
-document.getElementById("demo1").innerHTML = input.value;
-
-var url = window.location.search.substring(1);
-document.getElementById("demo2").innerHTML = "<a href='"+url+"'>About</a>";
-
-
-
-
-
+document.getElementById("demo1").innerHTML = location.hash.match(/demo1=([^;&]*)/)[1];
 
 document.getElementById("demo3").innerHTML = 5;
 document.getElementById("demo4").innerHTML = "<a href='/about.html'>About</a>";
-//document.getElementById("demo5").innerHTML = escapeHTML`<a href='${url}'>About</a>`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (location.hash.match(/demo2=([^;&]*)/)[1]) {
+    location.href = location.hash.match(/demo2=([^;&]*)/)[1];
+}
